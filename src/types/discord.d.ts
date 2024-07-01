330c99539644bd654bf4569036180cb439e8180a
WHEREISDAN <dantavious.w20@gmail.com>
2024-06-30 23:39:47 -0400

    removed old unused file

--- src/types/discord.d.ts
+++
@@ -1,8 +0,0 @@
-import { Collection } from 'discord.js';
-import { Command } from './command';
-
-declare module 'discord.js' {
-  export interface Client {
-    commands: Collection<string, Command>;
-  }
-}
